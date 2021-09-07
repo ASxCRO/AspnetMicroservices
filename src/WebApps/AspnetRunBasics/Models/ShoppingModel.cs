86c0a610f98a5a6889d5b484f8a5be99fe6029fa
asxcro <antonio.suups@gmail.com>
2021-09-07 14:07:54 +0200

    client web app integration done

---
+++ src/WebApps/AspnetRunBasics/Models/ShoppingModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AspnetRunBasics.Models
+{
+    public class ShoppingModel
+    {
+        public string UserName { get; set; }
+        public BasketModel BasketWithProducts { get; set; }
+        public IEnumerable<OrderResponseModel> Orders { get; set; }
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Services/BasketService.cs
@@ -0,0 +1,24 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+    }
+}
